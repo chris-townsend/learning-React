a3a187fed33eda6c369b122693cd028e9ca197d8
chris-townsend <chris-townsend@live.co.uk>
2023-01-28 12:12:30 +0000

    Create a basic greeting component

---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+// function FunctionalGreeting() {
+   // return <h1>Hello from React!</h1>
+//}
+
+const FunctionalGreetingWithProps = (props) => {
+   console.log(props);
+   return <h1>Hello {props.name}, I see your {props.age} year's old. {props.greeting}</h1>;
+};
+
+export default FunctionalGreetingWithProps
+
+
